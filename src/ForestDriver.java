b79f999c04f582e8e5f5dfcee65ae159f160d478
TheAIBot <andreasuranus@hotmail.com>
2015-11-14 18:06:43 +0100

    added faster alternative to orderedbagwithoutrepetition

--- src/ForestDriver.java
+++ src/ForestDriver.java
@@ -4,38 +4,46 @@ import java.util.List;
 public class ForestDriver {
 
 	public static void main(String[] args) {
-		FasterOrderedBag fisk = new FasterOrderedBag();
-		OrderedBag lol = new OrderedBag();
-		List<Long> fisktimes = new ArrayList<Long>();
-		List<Long> loltimes = new ArrayList<Long>();
+
+		System.out.println("--Ordered Bags Test--");
+		testTimeForBags(new OrderedBag(), new FasterOrderedBag());
 		
-		for (int i = 0; i < 20000; i++) {
-			long startT = System.nanoTime();
-			fisk.addString(String.valueOf(i));
-			fisktimes.add(System.nanoTime() - startT);
-		}
-		
-		for (int i = 0; i < 20000; i++) {
-			long startT = System.nanoTime();
-			lol.addString(String.valueOf(i));
-			loltimes.add(System.nanoTime() - startT);
-		}
-		boolean fasterIsCorrect = true;
-		for (int i = 0; i < fisk.noOfElements(); i++) {
-			if (!fisk.getString(i).equals(lol.getString(i))) {
-				fasterIsCorrect = false;
-				break;
-			}
-		}
-		System.out.println("is faster correct: " + String.valueOf(fasterIsCorrect));
-		System.out.println("Faster: " + calculateAverage(fisktimes));
-		System.out.println("Normal: " + calculateAverage(loltimes));
-		
+		System.out.println("--Ordered Bags Without Repetition Test--");
+		testTimeForBags(new OrderedBagWithoutRepetitions(), new FasterOrderedBagWithoutRepetition());
 		
 		
 		
 		//ForestSimulation fSim = new ForestSimulation(120,300);
 		//fSim.run();
+	}
+	
+	private static void testTimeForBags(Bag normal, Bag Faster)
+	{
+		List<Long> fasterTimes = new ArrayList<Long>();
+		List<Long> normalTimes = new ArrayList<Long>();
+		
+		for (int i = 0; i < 20000; i++) {
+			long startT = System.nanoTime();
+			Faster.addString(String.valueOf(i));
+			fasterTimes.add(System.nanoTime() - startT);
+		}
+		
+		for (int i = 0; i < 20000; i++) {
+			long startT = System.nanoTime();
+			normal.addString(String.valueOf(i));
+			normalTimes.add(System.nanoTime() - startT);
+		}
+		boolean fasterIsCorrect = true;
+		for (int i = 0; i < Faster.noOfElements(); i++) {
+			if (!Faster.getString(i).equals(normal.getString(i))) {
+				fasterIsCorrect = false;
+				break;
+			}
+		}
+		System.out.println("is faster correct: " + String.valueOf(fasterIsCorrect));
+		System.out.println("Faster: " + calculateAverage(fasterTimes));
+		System.out.println("Normal: " + calculateAverage(normalTimes));
+		System.out.println("");
 	}
 
 	private static double calculateAverage(List<Long> marks) {
